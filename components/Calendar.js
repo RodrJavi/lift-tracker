e69b097e9faab9d536ebc9d218efd89e3529cd5b
RodrJavi <nb2javi@gmail.com>
2022-07-06 22:31:08 -0500

    Front page styling

---
+++ components/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div className='history'>
+    <h2>Calendar</h2>
+    <div className='c-placeholder'></div>
+    </div>
+  )
+}
+
+export default Calendar
